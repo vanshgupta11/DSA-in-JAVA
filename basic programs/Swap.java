9c4d31c8678b2ba96a484d6f1f2da0b190aff294
vanshgupta11 <vanshkumargupta11@gmail.com>
2025-11-03 10:03:44 +0530

    basic programs

---
+++ basic programs/Swap.java
@@ -0,0 +1,18 @@
+public class Swap {
+   public static void main(String[] args) {
+    int a = 10 ;
+    int b = 20 ;
+
+    System.out.println(a);
+     System.out.println(b);
+
+     a = a + b ;
+      b = a-b;
+     a= a-b ;
+     
+     System.out.println(a);
+    System.out.println(b);
+
+   }
+
+}
