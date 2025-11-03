f451e82aef0d6bad367d356e911420fb1759f080
vanshgupta11 <vanshkumargupta11@gmail.com>
2025-11-03 09:48:56 +0530

    tempconvertor

---
+++ arrays/Tempconvertot.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+
+
+public class Tempconvertot{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter the temp in c");
+        float tempC = sc.nextFloat();
+        float tempF = (tempC * 9/5) + 32 ;
+        System.out.println(tempF);
+    }
+}
