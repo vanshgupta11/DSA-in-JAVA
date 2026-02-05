d8d87df7032887d5bc16fd784002d157fa78037f
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-02-05 14:13:49 +0530

    Pg4

---
+++ basic recursion/Pg4.java
@@ -0,0 +1,19 @@
+public class Pg4 {
+    public static void main(String[] args) {
+        int N = 10;
+        Solution sol = new Solution();
+        System.out.println(sol.sumOfNumbers(N));
+        
+        
+    }
+}
+ class Solution{
+    public int sumOfNumbers(int N){
+
+        if(N == 1){
+            return 1;
+        }
+        
+        return N + sumOfNumbers(N -1 );
+    }
+ }
