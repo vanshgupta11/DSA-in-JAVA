ab0ce29522622115aeb94fc7a62a3afa08c71f8a
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-02-05 11:40:24 +0530

    r3

---
+++ basic recursion/Pg3.java
@@ -0,0 +1,18 @@
+public class Pg3 {
+    public static void main(String[] args) {
+         int N = 10;
+        Solution sol = new Solution() ;
+        sol.printNumber(N);
+    }
+}
+
+class Solution{
+    public void printNumber(int current){
+        if(current <1)
+            return;
+
+        System.out.println(current);
+
+        printNumber(current -1);
+    }
+}
