0b018ebff6a3374a516a0efe6539fc5ad2a47c12
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-02-09 01:12:19 +0530

    arr3

---
+++ arrays/Input.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [][] arr = new int [3][3];
+        for(int row = 0; row < arr.length; row++ ){
+            for(int col = 0; col < arr[row].length; col++){
+                    arr[row][col] = sc.nextInt();
+                            }
+        }
+
+        for(int row = 0; row<arr.length;row++){
+            for(int col= 0 ;col<arr[row].length;col++){
+                System.out.print(arr[row][col]);
+            }
+        }
+    }
+}
