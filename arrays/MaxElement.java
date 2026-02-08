400550a67d5126ac9dc95616c00a45c3b9c7f63e
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-02-09 02:31:33 +0530

    MAxVal

---
+++ arrays/MaxElement.java
@@ -0,0 +1,15 @@
+public class MaxElement {
+ public static void main(String[] args) {
+    int[] arr = {1,9,23,5,7,25};
+        System.out.println(max(arr));
+ }   
+ static int max(int[] arr){
+    int maxVal = arr[0];
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]>maxVal){
+            maxVal = arr[i];
+        }
+    }
+    return maxVal;
+ }
+}
