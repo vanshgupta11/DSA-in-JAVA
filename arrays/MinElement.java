d5600fff93b327ad23eb351d81a8963f06e39480
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-02-09 19:14:18 +0530

    Min Element

---
+++ arrays/MinElement.java
@@ -0,0 +1,15 @@
+public class MinElement {
+    public static void main(String[] args) {
+        int[] arr = {18,12,-7,3,4,14,28};
+        System.out.println(min(arr));
+    }
+    static int min(int[]arr){
+        int minVal = arr[0];
+        for(int i = 0; i<arr.length;i++){
+            if(arr[i]<minVal){
+                minVal = arr[i];
+            }
+        }
+        return minVal;
+    }
+}
