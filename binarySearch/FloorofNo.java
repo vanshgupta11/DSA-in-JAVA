78b922de4ec7b81450ea4ad388fa59a7f2051ba0
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-02-12 01:01:52 +0530

    Floor of a number

---
+++ binarySearch/FloorofNo.java
@@ -0,0 +1,30 @@
+public class FloorofNo {
+    
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,8,9};
+        int target = 3;
+        System.out.println(binary(arr,target));
+        
+    }
+
+    static int binary(int[] arr , int target){
+        int start = 0;
+        int end = arr.length -1;
+
+        while(start <= end){
+            int mid = start + (end - start) /2;
+
+            if(target < arr[mid]){
+                end = mid -1 ;
+            }
+            else if(target > arr[mid]){
+                start = mid +1 ;
+            }
+            else{
+                return mid ;
+            }
+
+        }
+        return start;
+    }
+}
