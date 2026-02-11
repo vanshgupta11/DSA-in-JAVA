7a39cf0052c826efaf6d511505f45438c897f132
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-02-12 01:11:58 +0530

    con

--- binarySearch/CellingOfNo.java
+++ binarySearch/CellingOfNo.java
@@ -6,6 +6,9 @@ public class CellingOfNo {
         
     }
     static int binary(int[] arr , int target){
+        if(target > arr[arr.length-1]){
+            return -1;
+        }
          int start = 0;
         int end = arr.length -1;
           while(start <= end){
