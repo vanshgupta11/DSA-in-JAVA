7e110c8b7151f4722cd7b609e5f7f946a108f451
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-06-01 11:04:56 +0530

    Factorial - recurssion

---
+++ resursion/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(fact(6));
+        
+    }
+    static int fact(int n ){
+        if ( n < 1){
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+    }
+}
