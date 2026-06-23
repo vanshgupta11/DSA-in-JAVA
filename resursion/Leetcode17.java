500210b984e6ea9a236cc5638764f41a4a0fafd5
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-06-23 19:00:38 +0530

    count

---
+++ resursion/Leetcode17.java
@@ -0,0 +1,17 @@
+public class Leetcode17 {
+    public static void main(String[] args) {
+        pad("","12");
+    }
+    static void pad(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return ;
+        }
+        int digit = up.charAt(0) -'0';
+        for(int  i = (digit -1)*3 ; i < digit*3;i++){
+            char ch = (char)('a' + i);
+            pad(p + ch , up.substring(1));
+            int count++;
+        }
+    }
+}
